f4cd4cd345d2ffbddea1f156803c916f8d2d7564
schaevs <aschaevitz15@gmail.com>
2017-07-21 16:04:07 -0700

    alphaOCR w 93.39 accuracy using k = 3

--- alphaOCRkNN.py
+++ alphaOCRkNN.py
@@ -2,7 +2,20 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
-samplesDir = '/home/alexanderschaevitz/opencv/samples/cpp/'
+samplesDir = '/home/alexanderschaevitz/opencv/samples/data/'
 data = np.loadtxt( samplesDir + 'letter-recognition.data', dtype = 'float32', 
                    delimiter = ',', converters = {0: lambda ch: ord(ch)-ord('A')}
                  )
+
+train, test = np.vsplit(data,2)
+
+responses, trainData = np.hsplit(train,[1])
+labels,     testData = np.hsplit(test,[1])
+
+knn = cv2.KNearest()
+knn.train(trainData, responses)
+ret, result, neighbours, dist = knn.find_nearest(testData, k=3)
+
+correct = np.count_nonzero(result == labels)
+accuracy = correct*100.0/10000
+print accuracy
